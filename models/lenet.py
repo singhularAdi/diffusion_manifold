873c68545218f5c7de808ccb572c0e55c15272d4
Aditya Pratap Singh <adi18jan1999@gmail.com>
2022-11-29 16:55:11 -0500

    add classification model training

--- models/lenet.py
+++ models/lenet.py
@@ -2,19 +2,21 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 import pytorch_lightning as pl
+from torchmetrics.functional import accuracy
+
 
 class LeNet(nn.Module):
     def __init__(self):
         super(LeNet, self).__init__()
-        self.conv1 = nn.Conv2d(1, 6, (5,5), padding=2)
-        self.conv2 = nn.Conv2d(6, 16, (5,5))
-        self.fc1   = nn.Linear(16*5*5, 120)
-        self.fc2   = nn.Linear(120, 84)
-        self.fc3   = nn.Linear(84, 10)
+        self.conv1 = nn.Conv2d(1, 6, (5, 5), padding=2)
+        self.conv2 = nn.Conv2d(6, 16, (5, 5))
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
-        x = F.max_pool2d(F.relu(self.conv1(x)), (2,2))
-        x = F.max_pool2d(F.relu(self.conv2(x)), (2,2))
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+        x = F.max_pool2d(F.relu(self.conv2(x)), (2, 2))
         x = x.view(-1, self.num_flat_features(x))
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
@@ -39,15 +41,22 @@ class LeNetPL(pl.LightningModule):
         x, y = batch
         y_pred = self.lenet(x)
         loss = self.loss(y_pred, y)
+        self.log("train_loss", loss)
         return loss
 
-    def valdation_step(self, batch, batch_idx):
-        ...
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        y_pred = self.lenet(x)
+        loss = self.loss(y_pred, y)
+        class_preds = torch.argmax(y_pred, dim=1)
+        self.log("val_loss", loss)
+        self.log("val_acc", accuracy(y_pred, y))
+
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
         return optimizer
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     net = LeNet()
--- train.py
+++ train.py
@@ -1,3 +1,7 @@
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint
+
+from utils import get_loaders
 from models import LeNet, LeNetPL
 
 
@@ -5,5 +9,28 @@ def main():
     lenet = LeNet()
     model = LeNetPL(lenet)
 
-if __name__=='__main__':
+    # load data
+    trainloader, testloader = get_loaders()
+
+    checkpoint_acc = ModelCheckpoint(
+        save_top_k=1,
+        monitor="val_acc",
+        mode="max",
+        filename="amnist-{epoch:02d}-{val_loss:.4f}-{val_acc:.4f}"
+    )
+    checkpoint_loss = ModelCheckpoint(
+        save_top_k=1,
+        monitor="val_loss",
+        mode="min",
+        filename="lmnist-{epoch:02d}-{val_loss:.4f}-{val_acc:.4f}"
+    )
+    # train model
+
+    trainer = pl.Trainer(accelerator='gpu', max_epochs=50,
+                         callbacks=[checkpoint_acc, checkpoint_loss])
+    trainer.fit(model=model, train_dataloaders=trainloader,
+                val_dataloaders=testloader)
+
+
+if __name__ == '__main__':
     main()
