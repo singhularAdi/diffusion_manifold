a3f0f05051e096fa208a88cf057aab12c12b94a1
Aditya Pratap Singh <adi18jan1999@gmail.com>
2022-11-30 15:46:34 -0500

    add path finding algorithm; fin

--- find_path.py
+++ find_path.py
@@ -1,8 +1,12 @@
+import torch
+import torch.nn.functional as F
 from torch.linalg import norm
 from torch.nn.functional import log_softmax
 from functorch import jacrev
+import matplotlib.pyplot as plt
+from tqdm import tqdm, trange
 
-from utils import get_data, project_subspace, gram_schmidt_orthogonalize
+from utils import get_data, project_subspace
 from models import LeNet, LeNetPL
 
 
@@ -11,31 +15,57 @@ def predict(model, x):
     return log_softmax(pred)
 
 
-def find_path(s, d, model, alpha=0.1, T=5000):
+def find_path(s, d, model, alpha=0.1, T=10000, save_path=False):
+
+    x_hist = []
     x_t = s
 
-    for t in range(T):
-        # calculate jacobian of log_softmax wrt x_t
-        j = jacrev(predict, argnums=1)(model.lenet, x_t.unsqueeze(0))
-        j = j.squeeze()
+    plt.imshow(x_t.view(28, 28).detach().cpu().numpy(), cmap='gray')
+    plt.show()
+    plt.imshow(d.view(28, 28).detach().cpu().numpy(), cmap='gray')
+    plt.show()
 
-        # orthonomalize for projection
-        j = gram_schmidt_orthogonalize(j)
-        # project d-x_t onto j
-        v = project_subspace(d - x_t, j)
+    with torch.no_grad(), trange(T, desc='Traversing') as pbar:
+        for t in pbar:  # calculate jacobian of log_softmax wrt x_t
+            j = jacrev(predict, argnums=1)(model.lenet, x_t.unsqueeze(0))
+            j = j.squeeze()
 
-        # update
-        x_t = x_t + alpha * (v / norm(v))
+            # orthonomalize for projection
+            # j = gram_schmidt_orthogonalize(j)
+
+            # TODO check need for 10 * -1 shapes, does 10* 28* 28 make a difference?
+            # orthogonalize
+            j_orth, _ = torch.linalg.qr(j.view(j.shape[0], -1).T)
+
+            # project d-x_t onto j
+            v = project_subspace(d - x_t, j_orth.T)
+
+            # update
+            x_t = x_t + alpha * (v.view(x_t.shape) / norm(v))
+
+            # book keeping
+            if t % 1000 == 0:
+                probs = F.softmax(model.lenet(x_t.unsqueeze(0)))
+                idx = torch.argmax(probs)
+                norm_diff = norm(d - x_t)
+                pbar.set_postfix(norm_diff=norm_diff.item(),
+                                    p_class=idx.item(),
+                                    prob=probs[0][idx].item())
+
+                if save_path:
+                    x_hist.append(x_t.view(28, 28).detach().cpu().numpy())
+
+    plt.imshow(x_t.view(28, 28).detach().cpu().numpy(), cmap='gray')
+    plt.show()
 
 
 def main():
-
     checkpoint_path = '/home/aditya/workspace/diffusion_manifold/lightning_logs/version_0/checkpoints/lmnist-epoch=20-val_loss=0.0303-val_acc=0.9914.ckpt'
 
     # load dataset, get source image and destination image
     tdata, vdata = get_data()
-    s, sl = tdata[0]
-    d, dl = tdata[1]
+    s, _ = tdata[3]
+    d, _ = tdata[0]
 
     # load model
     lenet = LeNet()
--- utils/vectors.py
+++ utils/vectors.py
@@ -1,11 +1,52 @@
+import torch
 
 
-def project_on_one():
-    ...
+def project_on_one(x, y):
+    '''Project x onto y'''
+    mag = torch.dot(x, y)
+    vec = mag * (y / torch.linalg.norm(y))
+    return vec
+
 
 def gram_schmidt_orthogonalize():
     ...
 
-def project_subspace():
-    ...
 
+def project_subspace(delta, j):
+    proj = torch.zeros_like(delta.view(-1))
+
+    for i in range(j.shape[0]):
+        proj += project_on_one(delta.view(-1), j[i].view(-1))
+
+    return proj
+
+
+def gram_schmidt(vv):
+    def projection(u, v):
+        return (v * u).sum() / (u * u).sum() * u
+
+    nk = vv.size(0)
+    uu = torch.zeros_like(vv, device=vv.device)
+    uu[:, 0] = vv[:, 0].clone()
+    for k in range(1, nk):
+        vk = vv[k].clone()
+        uk = 0
+        for j in range(0, k):
+            uj = uu[:, j].clone()
+            uk = uk + projection(uj, vk)
+        uu[:, k] = vk - uk
+    for k in range(nk):
+        uk = uu[:, k].clone()
+        uu[:, k] = uk / uk.norm()
+    return uu
+
+
+if __name__ == '__main__':
+    import ipdb; ipdb.set_trace()
+    a = torch.randn(5, 5, requires_grad=True)
+
+    b = gram_schmidt(a)
+    c = b.sum()
+    c.backward()
+    print(b.matmul(b.t()))
+    print(a.grad)
