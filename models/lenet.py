14fe7f5858456ac17015a5506b610f0bcc47c44b
Aditya Pratap Singh <adi18jan1999@gmail.com>
2022-12-14 22:54:58 -0500

    updated lenet to use 64x64 imgs

--- models/lenet.py
+++ models/lenet.py
@@ -10,9 +10,9 @@ class LeNet(nn.Module):
         super(LeNet, self).__init__()
         self.conv1 = nn.Conv2d(1, 6, (5, 5), padding=2)
         self.conv2 = nn.Conv2d(6, 16, (5, 5))
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.fc1 = nn.Linear(16 * 14 * 14, 512)
+        self.fc2 = nn.Linear(512, 128)
+        self.fc3 = nn.Linear(128, 10)
 
     def forward(self, x):
         x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint
 
-from utils import get_loaders
+from utils import get_loaders2
 from models import LeNet, LeNetPL
 
 
@@ -10,24 +10,18 @@ def main():
     model = LeNetPL(lenet)
 
     # load data
-    trainloader, testloader = get_loaders()
+    trainloader, testloader = get_loaders2()
 
-    checkpoint_acc = ModelCheckpoint(
-        save_top_k=1,
-        monitor="val_acc",
-        mode="max",
-        filename="amnist-{epoch:02d}-{val_loss:.4f}-{val_acc:.4f}"
-    )
     checkpoint_loss = ModelCheckpoint(
         save_top_k=1,
         monitor="val_loss",
         mode="min",
-        filename="lmnist-{epoch:02d}-{val_loss:.4f}-{val_acc:.4f}"
+        filename="mnist64-{epoch:02d}-{val_loss:.4f}-{val_acc:.4f}"
     )
     # train model
 
     trainer = pl.Trainer(accelerator='gpu', max_epochs=50,
-                         callbacks=[checkpoint_acc, checkpoint_loss])
+                         callbacks=[checkpoint_loss])
     trainer.fit(model=model, train_dataloaders=trainloader,
                 val_dataloaders=testloader)
 
